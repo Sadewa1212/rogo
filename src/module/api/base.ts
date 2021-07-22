184739a45e52800e5bac44fbb268ac94eaee3e5a
luongvantuit <luongvantuit@gmail.com>
2021-07-22 13:06:07 +0700

    config base callApi

--- src/module/api/base.ts
+++ src/module/api/base.ts
@@ -3,7 +3,25 @@ import { } from "react";
 
 
 function convertUri(uri: Uri): string {
-    return "";
+    /***
+     * convert type uri to RequestInfor
+     */
+    var _scheme: string = uri.scheme === undefined ? "http" : uri.scheme;
+    var _host: string = uri.host === undefined ? "localhost" : uri.host;
+    var _port: string = uri.port === undefined ? "" : `:${uri.port}`;
+    var _path: string = "";
+    var _queryParameter: string = "";
+    if (uri.path !== undefined && uri.path.length !== 0)
+        uri.path.forEach(element => {
+            _path += `/${element}`
+        })
+    if (uri.queryParameter !== undefined && uri.queryParameter.length !== 0) {
+        uri.queryParameter.forEach(element => {
+            _queryParameter += `&${element.key}=${element.value}`
+        })
+        _queryParameter = `?${_queryParameter.substring(1)}`
+    }
+    return `${_scheme}://${_host}${_port}${_path}${_queryParameter}`;
 }
 
 
--- src/module/type/uri.ts
+++ src/module/type/uri.ts
@@ -1,5 +1,12 @@
 interface Uri {
-
+    scheme?: string | undefined;
+    host?: string | undefined;
+    port?: number | undefined;
+    path?: Array<string> | undefined;
+    queryParameter?: Array<{
+        key: string;
+        value: string;
+    }> | undefined;
 }
 export type {
     Uri,
