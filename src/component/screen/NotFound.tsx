1694dae00407c9f6124c7cd278d40745a1f4de63
luongvantuit <luongvantuit@gmail.com>
2021-07-24 12:36:18 +0700

    add config base not found page

--- src/component/screen/NotFound.tsx
+++ src/component/screen/NotFound.tsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import { UnDrawPageNotFound } from '../../asset'
-
+import "../../sass/NotFound.scss"
 export default function NotFound() {
     return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-        }}>
-            <img src={UnDrawPageNotFound} alt="Page Not Found" style={
-                {
-                    width: "50%",
-                    height: "50%",
-                }
-            } />
-            <p>Page Not Found</p>
+        <div className="NotFoundDiv">
+            <img src={UnDrawPageNotFound} alt="Page Not Found" className="NotFoundDivImgUndrawPageNotFound" />
+            <p className="NotFoundP">Page Not Found</p>
         </div>
     )
 }
