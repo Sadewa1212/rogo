c5a25dcfccf654da765d963d798a5c3be841a2ca
luongvantuit <luongvantuit@gmail.com>
2021-07-25 12:32:37 +0700

    add undraw loading zzz

--- src/asset/index.ts
+++ src/asset/index.ts
@@ -1,5 +1,6 @@
 import UnDrawPageNotFound from "./undraw_page_not_found.svg";
-
+import UnDrawLoadingZZZ from "./undraw_loading_zzz.svg";
 export {
     UnDrawPageNotFound,
+    UnDrawLoadingZZZ,
 }
