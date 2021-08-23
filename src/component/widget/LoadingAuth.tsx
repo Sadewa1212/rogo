a55b45117a4d3c82f94a0cf2128e77026b170203
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-23 21:13:52 +0700

    test class

--- src/component/widget/LoadingAuth.tsx
+++ src/component/widget/LoadingAuth.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { UnDrawSynchronize } from '../../asset'
 
-export default function LoadingAuth() {
-    return (
-        <div className='loading_auth'>
-            <img src={UnDrawSynchronize} alt='Loading'/>
-        </div>
-    )
+export default class LoadingAuth extends React.Component<any> {
+    render() {
+        return (
+            <div className='loading_auth'>
+                <img src={UnDrawSynchronize} alt='Loading' />
+            </div>
+        );
+    }
 }
