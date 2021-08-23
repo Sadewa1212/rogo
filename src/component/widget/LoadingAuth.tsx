4fad23c0b51a04d76a4ed23531aecf02031e7f0b
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-23 15:26:56 +0700

    loading base

--- src/component/widget/LoadingAuth.tsx
+++ src/component/widget/LoadingAuth.tsx
@@ -4,7 +4,9 @@ import { UnDrawSynchronize } from '../../asset'
 export default function LoadingAuth() {
     return (
         <div className='loading_auth'>
-            <img src={UnDrawSynchronize} />
+            <div className='loading_auth_box'>
+                <img src={UnDrawSynchronize} alt='Loading'/>
+            </div>
         </div>
     )
 }
