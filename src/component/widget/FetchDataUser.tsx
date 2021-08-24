94c73968e5d577d7a86fe40a41c48ed28a403ca2
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-24 14:30:12 +0700

    fetch

--- src/component/widget/FetchDataUser.tsx
+++ src/component/widget/FetchDataUser.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { UnDrawNoData } from "../../asset";
 
 export default class FetchDataUser extends React.Component {
 
+    componentDidMount() {
+        document.title = 'Loading...'
+    }
+
     render(): JSX.Element {
         return (
-            <div>
-
+            <div className='fetch_data_user'>
+                <div className='fetch_data_user_box'>
+                    <img src={UnDrawNoData} alt='No Data' />
+                    <div className='fetch_data_user_process'>
+                        <div className='fetch_data_user_process_nested' />
+                    </div>
+                </div>
             </div>
         );
     }
