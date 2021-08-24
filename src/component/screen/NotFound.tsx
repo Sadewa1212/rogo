2a552eefe4441bf80b6ade38a948f563d3dc4f3b
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-24 08:24:02 +0700

    class

--- src/component/screen/NotFound.tsx
+++ src/component/screen/NotFound.tsx
@@ -1,9 +1,21 @@
+import React from 'react';
 import { UnDrawPageNotFound } from '../../asset'
-export default function NotFound() {
-    return (
-        <div className="not_found">
-            <img src={UnDrawPageNotFound} alt="Page Not Found" />
-            <p>Page Not Found</p>
-        </div>
-    )
+export default class NotFound extends React.Component<any, any>{
+
+    componentDidMount() {
+        document.title = 'Page Not Found'
+    }
+
+    componentDidUpdate() {
+        document.title = 'Page Not Found'
+    }
+
+    render() {
+        return (
+            <div className="not_found">
+                <img src={UnDrawPageNotFound} alt="Page Not Found" />
+                <p>Page Not Found</p>
+            </div>
+        );
+    }
 }
