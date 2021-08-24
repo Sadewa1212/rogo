cc8e502f0a3bb9d7dfd485f04fe6ae17ed2067a0
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-24 08:27:36 +0700

    data struc

--- src/component/screen/NotFound.tsx
+++ src/component/screen/NotFound.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { UnDrawPageNotFound } from '../../asset'
 export default class NotFound extends React.Component<any, any>{
 
-    componentDidMount() {
+    componentDidMount(): void {
         document.title = 'Page Not Found'
     }
 
-    componentDidUpdate() {
+    componentDidUpdate(): void {
         document.title = 'Page Not Found'
     }
 
-    render() {
+    render(): JSX.Element {
         return (
             <div className="not_found">
                 <img src={UnDrawPageNotFound} alt="Page Not Found" />
