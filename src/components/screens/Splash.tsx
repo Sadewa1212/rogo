8ae2c016cfea6da383b05a0448270baa363b7fbd
Lương Văn Tú <87352056+luongvantuit@users.noreply.github.com>
2021-10-10 10:15:26 +0700

    prod nginx config failed

--- src/components/screens/Splash.tsx
+++ src/components/screens/Splash.tsx
@@ -3,5 +3,5 @@ import React from "react";
 //import { useAuth } from '../context'
 export default function Splash() {
   //const auth: AuthState = useAuth();
-  return <div>{process.env.REACT_APP_NODE_ENV}</div>;
+  return <div>{process.env.REACT_APP_API_KEY}</div>;
 }
